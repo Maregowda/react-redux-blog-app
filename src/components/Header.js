dc6e0da79cadf71cae831ac4a974b4e72833f5de
maregowdac <maregowdac6979@gmail.com>
2023-08-26 21:22:58 +0530

    changed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
 
   return (
     <header className="Header">
-        <h1>Author Blog</h1>
+        <h1>Author's Blog</h1>
         <nav>
             <ul>
                 <li><Link to="/">Home</Link></li>
